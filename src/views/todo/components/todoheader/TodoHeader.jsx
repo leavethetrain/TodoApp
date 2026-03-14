70457dafe416cfba7b4847a4d269f5aca28ff56f
leavethetrain <musik201092@web.de>
2026-03-14 19:27:42 +0100

    add warning

--- src/views/todo/components/todoheader/TodoHeader.jsx
+++ src/views/todo/components/todoheader/TodoHeader.jsx
@@ -2,18 +2,37 @@ import Input from "../../../../components/input/Input";
 import Button from "../../../../components/button/Button";
 import { useState } from "react";
 import "./TodoHeader.css";
+import piep from "./piep.mp3";
 function TodoHeader({ AddTodoItemToList }) {
   const [inputValue, setInputValue] = useState("");
+  const [showWarning, setShowWarning] = useState(false);
+
   function handleInputChangeEvent(event) {
     setInputValue(event.target.value);
     console.log(event.target.value);
   }
   function handleAddTodoItemEvent() {
+    if (inputValue.trim() === "") {
+      setShowWarning(true);
+
+      const audio = new Audio(piep);
+      audio.play();
+      return;
+    }
+
     AddTodoItemToList({ id: Math.random(), content: inputValue, done: false });
     setInputValue("");
   }
   return (
     <div className="todo-header">
+      {showWarning && (
+        <div className="warning-overlay">
+          <div className="warning-box">
+            <p>Bitte Todo eingeben</p>
+            <button onClick={() => setShowWarning(false)}>OK</button>
+          </div>
+        </div>
+      )}
       <Input
         handleInputChangeEvent={handleInputChangeEvent}
         inputValue={inputValue}
